21b7da4bf5161214fcbdd1918028cc75e7a03cdb
Hardcodepunk <howl@hardcodepunk.be>
2025-03-16 18:33:38 +0530

    styling info section

--- src/components/templates/InfoSection/InfoSection.tsx
+++ src/components/templates/InfoSection/InfoSection.tsx
@@ -3,7 +3,15 @@
 import { StyledSection } from "./InfoSection.styles"
 
 function InfoSection() {
-  return <StyledSection></StyledSection>
+  return (
+    <StyledSection>
+      <h2>Custom AI Viking Videos</h2>
+      <p>
+        Your real likeness into one-of-a-kind Viking-inspired videos that captivate and resonate. Your natural beauty
+        amplified.
+      </p>
+    </StyledSection>
+  )
 }
 
 export default InfoSection
