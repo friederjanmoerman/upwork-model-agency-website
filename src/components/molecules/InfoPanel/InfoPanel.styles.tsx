aa4ac952ce81387ee0ed685ff15f6778d4ebe452
Hardcodepunk <howl@hardcodepunk.be>
2025-03-19 17:57:06 +0530

    initial anim infopanel

--- src/components/molecules/InfoPanel/InfoPanel.styles.tsx
+++ src/components/molecules/InfoPanel/InfoPanel.styles.tsx
@@ -1,7 +1,13 @@
-"use client"
-
+// InfoPanel.styles.tsx
 import { styled } from "@mui/material"
 
-export const StyledPanel = styled("div")(() => ({
+interface StyledPanelProps {
+  visible: boolean
+}
+
+export const StyledPanel = styled("div")<StyledPanelProps>(({ visible }) => ({
   flex: "0 0 45%",
+  opacity: visible ? 1 : 0,
+  transform: visible ? "translateY(0)" : "translateY(40px)",
+  transition: "opacity 0.8s ease, transform 0.8s ease",
 }))
--- src/components/molecules/InfoPanel/InfoPanel.tsx
+++ src/components/molecules/InfoPanel/InfoPanel.tsx
@@ -1,11 +1,33 @@
-import { Typography } from "@mui/material"
+// InfoPanel.tsx
+"use client"
+
+import React, { useEffect, useRef, useState } from "react"
+import Typography from "@mui/material/Typography"
 import { StyledPanel } from "./InfoPanel.styles"
 
 function InfoPanel() {
+  const panelRef = useRef<HTMLDivElement>(null)
+  const [isVisible, setIsVisible] = useState(false)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting)
+      },
+      { threshold: 0.1 } // Adjust threshold as necessary
+    )
+
+    if (panelRef.current) observer.observe(panelRef.current)
+
+    return () => {
+      if (panelRef.current) observer.unobserve(panelRef.current)
+    }
+  }, [])
+
   return (
     <>
-      <StyledPanel></StyledPanel>
-      <StyledPanel>
+      <StyledPanel ref={panelRef} visible={isVisible}></StyledPanel>
+      <StyledPanel ref={panelRef} visible={isVisible}>
         <Typography variant="h2">Custom AI Viking Videos</Typography>
         <Typography color="primary" variant="body1">
           Your real likeness into one-of-a-kind Viking-inspired videos that captivate and resonate. Your natural beauty
